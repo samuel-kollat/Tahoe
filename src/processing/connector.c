00a25282f8b8a562b824688521355cf4fd9b666b
Samuel <samuel.kollat@gmail.com>
2015-02-04 11:19:32 +0100

    online queue

--- src/processing/connector.c
+++ src/processing/connector.c
@@ -1,2 +1,70 @@
 #include "connector.h"
 
+void PrintMeErrorMessage(char* dst, char* msg)
+{
+    fprintf(stderr, "Error in Connector (%s): %s\n", dst, msg);
+    return;
+}
+
+TMeStatus SetTypeOfQueue(TQueueType type,
+    TQueueParam param, TQueue** queue)
+{
+    switch(type)
+    {
+        case ONLINE:
+            *queue = GetOnlineQueue(param);
+            break;
+        case OFFLINE:
+            *queue = GetOfflineQueue(param);
+            break;
+        default:
+            PrintMeErrorMessage("SetTypeOfQueue", "wrong type of queue");
+            return ME_FAIL;
+    }
+
+    if(*queue == NULL)
+    {
+        PrintMeErrorMessage("SetTypeOfQueue", "cannot get queue");
+        return ME_FAIL;
+    }
+
+    return ME_OK;
+}
+
+TQueue* GetOnlineQueue(TQueueParam param)
+{
+    TQueue* queue = (TQueue*)(malloc(sizeof(TQueue)));
+    if(queue == NULL)
+    {
+        PrintMeErrorMessage("GetOnlineQueue", "malloc");
+        return NULL;
+    }
+
+    queue->type = ONLINE;
+    queue->head = CreateBackstop();
+    queue->pcap = NULL;
+
+    return queue;
+}
+
+TQueueItem* CreateBackstop()
+{
+    TQueueItem* item = (TQueueItem*)(malloc(sizeof(TQueueItem)));
+    if(item == NULL)
+    {
+        PrintMeErrorMessage("CreateBackstop", "malloc");
+        return NULL;
+    }
+
+    item->packet = NULL;
+    item->next = NULL;
+    item->backstop = true;
+
+    return item;
+}
+
+
+TQueue* GetOfflineQueue(TQueueParam param)
+{
+    return NULL;
+}
--- src/processing/connector.h
+++ src/processing/connector.h
@@ -1,6 +1,9 @@
 #ifndef __MIDDLEND_CONNECTOR__
 #define __MIDDLEND_CONNECTOR__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "onep_dpss_callback_framework.h"
 
 // Status
@@ -21,15 +24,21 @@ typedef int TQueueParam;
 // Type of packet
 typedef struct onep_dpss_paktype_* TPacket;
 
+// Type of pcap
+typedef FILE* TPcap;
+
 // Item on packet queue
 typedef struct Packet {
     TPacket packet;
     struct Packet* next;
+    bool backstop;          // True if Backstop
 } TQueueItem;
 
 // Queue
 typedef struct {
-    TPacket head;    // Pointer to first element
+    TQueueType type;    // Type of the queue
+    TQueueItem* head;       // Pointer to first element
+    TPcap pcap;         // Pointer to pcap file
 } TQueue;
 
 // Type of callback to process queue
@@ -40,5 +49,17 @@ TMeStatus SetTypeOfQueue(TQueueType type, TQueueParam param, TQueue** queue);
 
 // Register callbact to be called on the set queue
 TMeStatus RegisterQueueCallback(TQueueCallback callback);
+
+// Print error
+void PrintMeErrorMessage(char* dst, char* msg);
+
+//
+TQueue* GetOnlineQueue(TQueueParam param);
+
+//
+TQueueItem* CreateBackstop();
+
+//
+TQueue* GetOfflineQueue(TQueueParam param);
 
 #endif
