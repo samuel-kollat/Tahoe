4408d7a77e9d7bf129762c4ebf3d3c08f7c2a0b9
David Antolik <david@antolik.eu>
2015-02-13 13:17:00 +0100

    db scheme update + test data

--- src/database/mysql/schema/schema.sql
+++ src/database/mysql/schema/schema.sql
@@ -2,10 +2,10 @@
 -- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Počítač: localhost
--- Vygenerováno: Čtv 05. úno 2015, 11:55
--- Verze serveru: 5.5.38-0ubuntu0.14.04.1
--- Verze PHP: 5.5.9-1ubuntu4.3
+-- Host: localhost
+-- Generation Time: Feb 13, 2015 at 01:14 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Databáze: `tahoe`
+-- Database: `tahoe`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `access_list`
+-- Table structure for table `access_list`
 --
 
 CREATE TABLE IF NOT EXISTS `access_list` (
@@ -45,10 +45,17 @@ CREATE TABLE IF NOT EXISTS `access_list` (
   KEY `fk_access_list_ports2_idx` (`pn_destination`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
+--
+-- Dumping data for table `access_list`
+--
+
+INSERT INTO `access_list` (`id`, `number`, `action`, `protocol`, `ip_source`, `ip_destination`, `ttl`, `filter_id`, `pn_source`, `pn_destination`) VALUES
+(3, NULL, 'permit', 'TCP,UDP', 1, 2, NULL, 1, 1, 0);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `analyzer`
+-- Table structure for table `analyzer`
 --
 
 CREATE TABLE IF NOT EXISTS `analyzer` (
@@ -57,12 +64,20 @@ CREATE TABLE IF NOT EXISTS `analyzer` (
   `description` varchar(45) DEFAULT NULL,
   `src` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `analyzer`
+--
+
+INSERT INTO `analyzer` (`id`, `name`, `description`, `src`) VALUES
+(1, 'VoIP', 'Analyzer for VoIP', '/var/analyzer/voip.c'),
+(2, 'HTTP', 'Analyzer for HTTP', '/var/analyzer/http.c');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `application`
+-- Table structure for table `application`
 --
 
 CREATE TABLE IF NOT EXISTS `application` (
@@ -74,12 +89,20 @@ CREATE TABLE IF NOT EXISTS `application` (
   PRIMARY KEY (`id`),
   KEY `fk_application_certificates1_idx` (`certificate_id`),
   KEY `fk_application_analyzer1_idx` (`analyzer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `application`
+--
+
+INSERT INTO `application` (`id`, `name`, `active_flag`, `certificate_id`, `analyzer_id`) VALUES
+(1, 'VoIP monitoring', NULL, 1, 1),
+(2, 'HTTP monitoring', NULL, 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `certificate`
+-- Table structure for table `certificate`
 --
 
 CREATE TABLE IF NOT EXISTS `certificate` (
@@ -89,10 +112,17 @@ CREATE TABLE IF NOT EXISTS `certificate` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `certificate`
+--
+
+INSERT INTO `certificate` (`id`, `name`, `root_cert_path`) VALUES
+(1, 'cert1', '/home/user/cert.pem');
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `filter`
+-- Table structure for table `filter`
 --
 
 CREATE TABLE IF NOT EXISTS `filter` (
@@ -104,10 +134,18 @@ CREATE TABLE IF NOT EXISTS `filter` (
   KEY `fk_policy_map_policy_map1_idx` (`application_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
+--
+-- Dumping data for table `filter`
+--
+
+INSERT INTO `filter` (`id`, `name`, `type`, `application_id`) VALUES
+(1, 'filter1 - http', NULL, 2),
+(3, 'filter2 - voip', NULL, 1);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `filter_has_nbar_protocol`
+-- Table structure for table `filter_has_nbar_protocol`
 --
 
 CREATE TABLE IF NOT EXISTS `filter_has_nbar_protocol` (
@@ -118,10 +156,21 @@ CREATE TABLE IF NOT EXISTS `filter_has_nbar_protocol` (
   KEY `fk_filter_has_nbar_protocol_filter1_idx` (`filter_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `filter_has_nbar_protocol`
+--
+
+INSERT INTO `filter_has_nbar_protocol` (`filter_id`, `nbar_protocol_id`) VALUES
+(3, 1),
+(1, 2),
+(1, 3),
+(3, 4),
+(3, 5);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `interface_list`
+-- Table structure for table `interface_list`
 --
 
 CREATE TABLE IF NOT EXISTS `interface_list` (
@@ -136,7 +185,7 @@ CREATE TABLE IF NOT EXISTS `interface_list` (
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `ip_network`
+-- Table structure for table `ip_network`
 --
 
 CREATE TABLE IF NOT EXISTS `ip_network` (
@@ -146,10 +195,18 @@ CREATE TABLE IF NOT EXISTS `ip_network` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
+--
+-- Dumping data for table `ip_network`
+--
+
+INSERT INTO `ip_network` (`id`, `address`, `mask`) VALUES
+(1, '192.168.1.100', 32),
+(2, '192.168.1.200', 32);
+
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `nbar_protocol`
+-- Table structure for table `nbar_protocol`
 --
 
 CREATE TABLE IF NOT EXISTS `nbar_protocol` (
@@ -158,12 +215,23 @@ CREATE TABLE IF NOT EXISTS `nbar_protocol` (
   `protocol_description` varchar(255) DEFAULT NULL,
   `protocol_id` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `nbar_protocol`
+--
+
+INSERT INTO `nbar_protocol` (`id`, `protocol_name`, `protocol_description`, `protocol_id`) VALUES
+(1, 'SIP', 'SIP description', 'SIP'),
+(2, 'HTTP', 'HTTP description', 'HTTP'),
+(3, 'DNS', 'DNS description', 'DNS'),
+(4, 'RTP', 'RTP description', 'RTP'),
+(5, 'RTCP', 'RTCP description', 'RTCP');
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `ports`
+-- Table structure for table `ports`
 --
 
 CREATE TABLE IF NOT EXISTS `ports` (
@@ -171,12 +239,22 @@ CREATE TABLE IF NOT EXISTS `ports` (
   `greater_or_equal` int(11) DEFAULT NULL,
   `less_or_equal` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `ports`
+--
+
+INSERT INTO `ports` (`id`, `greater_or_equal`, `less_or_equal`) VALUES
+(0, 0, 65535),
+(1, 80, 80),
+(2, 53, 53),
+(3, 443, 443);
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabulky `router`
+-- Table structure for table `router`
 --
 
 CREATE TABLE IF NOT EXISTS `router` (
@@ -192,47 +270,55 @@ CREATE TABLE IF NOT EXISTS `router` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
--- Omezení pro exportované tabulky
+-- Dumping data for table `router`
+--
+
+INSERT INTO `router` (`id`, `management_ip`, `name`, `application_id`, `username`, `password`, `interfaces`) VALUES
+(1, '10.10.10.1', 'R1', 1, 'cisco', 'cisco', 'GigabitEthernet0/0'),
+(2, '10.10.10.2', 'R2', 1, 'cisco2', 'cisco', 'GigabitEthernet0/1');
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Omezení pro tabulku `access_list`
+-- Constraints for table `access_list`
 --
 ALTER TABLE `access_list`
-  ADD CONSTRAINT `fk_access_list_ports1` FOREIGN KEY (`pn_source`) REFERENCES `ports` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_access_list_ports2` FOREIGN KEY (`pn_destination`) REFERENCES `ports` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_access_list_filter1` FOREIGN KEY (`filter_id`) REFERENCES `filter` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_access_list_ip_networks` FOREIGN KEY (`ip_source`) REFERENCES `ip_network` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_access_list_ip_networks1` FOREIGN KEY (`ip_destination`) REFERENCES `ip_network` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_access_list_ip_networks1` FOREIGN KEY (`ip_destination`) REFERENCES `ip_network` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_access_list_ports1` FOREIGN KEY (`pn_source`) REFERENCES `ports` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_access_list_ports2` FOREIGN KEY (`pn_destination`) REFERENCES `ports` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Omezení pro tabulku `application`
+-- Constraints for table `application`
 --
 ALTER TABLE `application`
   ADD CONSTRAINT `fk_application_analyzer1` FOREIGN KEY (`analyzer_id`) REFERENCES `analyzer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_application_certificates1` FOREIGN KEY (`certificate_id`) REFERENCES `certificate` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Omezení pro tabulku `filter`
+-- Constraints for table `filter`
 --
 ALTER TABLE `filter`
   ADD CONSTRAINT `fk_policy_map_policy_map1` FOREIGN KEY (`application_id`) REFERENCES `application` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Omezení pro tabulku `filter_has_nbar_protocol`
+-- Constraints for table `filter_has_nbar_protocol`
 --
 ALTER TABLE `filter_has_nbar_protocol`
   ADD CONSTRAINT `fk_filter_has_nbar_protocol_filter1` FOREIGN KEY (`filter_id`) REFERENCES `filter` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_filter_has_nbar_protocol_nbar_protocol1` FOREIGN KEY (`nbar_protocol_id`) REFERENCES `nbar_protocol` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Omezení pro tabulku `interface_list`
+-- Constraints for table `interface_list`
 --
 ALTER TABLE `interface_list`
   ADD CONSTRAINT `fk_interface_list_router1` FOREIGN KEY (`router_id`) REFERENCES `router` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Omezení pro tabulku `router`
+-- Constraints for table `router`
 --
 ALTER TABLE `router`
   ADD CONSTRAINT `fk_router_application1` FOREIGN KEY (`application_id`) REFERENCES `application` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
